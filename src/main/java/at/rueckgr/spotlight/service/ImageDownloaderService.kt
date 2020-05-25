0257c945a828fc5d4e28f4bbfa8cea653eab880b
Paul Staroch <paulchen@rueckgr.at>
2020-05-25 23:08:12 +0200

    Migrate ImageDownloaderService to Kotlin

---
+++ src/main/java/at/rueckgr/spotlight/service/ImageDownloaderService.kt
@@ -0,0 +1,21 @@
+package at.rueckgr.spotlight.service
+
+import at.rueckgr.spotlight.model.DownloadableImage
+import at.rueckgr.spotlight.util.SpotlightException
+import org.apache.commons.io.IOUtils
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+
+class ImageDownloaderService {
+    fun fetchImage(downloadableImage: DownloadableImage): ByteArray {
+        val httpClient = HttpClients.custom().disableDefaultUserAgent().build()
+
+        httpClient.execute(HttpGet(downloadableImage.url)).use {
+            val statusCode = it.statusLine.statusCode
+            if (statusCode != 200) {
+                throw SpotlightException(String.format("Fail to retrieve image content: %s", statusCode))
+            }
+            return IOUtils.toByteArray(it.entity.content)
+        }
+    }
+}
