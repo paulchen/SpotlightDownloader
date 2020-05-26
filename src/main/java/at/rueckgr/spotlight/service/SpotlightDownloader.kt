9e373869f25225f838c8c0e677f8665ecf10f352
Paul Staroch <paulchen@rueckgr.at>
2020-05-26 21:15:00 +0200

    Exit with non-zero exit code on error

--- src/main/java/at/rueckgr/spotlight/service/SpotlightDownloader.kt
+++ src/main/java/at/rueckgr/spotlight/service/SpotlightDownloader.kt
@@ -1,27 +1,33 @@
 package at.rueckgr.spotlight.service
 
 import org.apache.logging.log4j.LogManager
+import kotlin.system.exitProcess
 
 fun main() {
-    val log = LogManager.getLogger("Main")
+    try {
+        val log = LogManager.getLogger("Main")
 
-    val iterations = 100
+        val iterations = 100
 
-    val localesService = LocalesService()
-    val metricsService = MetricsService()
-    val metadataService = MetadataService(localesService)
-    val downloadDirectoryService = DownloadDirectoryService(metadataService, metricsService)
-    metadataService.downloadDirectoryService = downloadDirectoryService
-    val imageDataDownloaderService = ImageDataDownloaderService(localesService, metricsService)
+        val localesService = LocalesService()
+        val metricsService = MetricsService()
+        val metadataService = MetadataService(localesService)
+        val downloadDirectoryService = DownloadDirectoryService(metadataService, metricsService)
+        metadataService.downloadDirectoryService = downloadDirectoryService
+        val imageDataDownloaderService = ImageDataDownloaderService(localesService, metricsService)
 
-    downloadDirectoryService.init()
-    val worker = Worker(imageDataDownloaderService, downloadDirectoryService)
-    log.info("Starting download")
-    for(i in 1..iterations) {
-        worker.run()
-        log.info("Iteration {}/{} done", i, iterations)
+        downloadDirectoryService.init()
+        val worker = Worker(imageDataDownloaderService, downloadDirectoryService)
+        log.info("Starting download")
+        for (i in 1..iterations) {
+            worker.run()
+            log.info("Iteration {}/{} done", i, iterations)
+        }
+
+        metadataService.saveMetadata()
+        metricsService.logMetrics()
     }
-
-    metadataService.saveMetadata()
-    metricsService.logMetrics()
+    catch (e: Throwable) {
+        exitProcess(1)
+    }
 }
