b907835f9a04383f158dd9f42c3195c26bfcb273
Paul Staroch <paulchen@rueckgr.at>
2024-07-03 17:20:53 +0200

    migrate to httpclient5

--- src/main/kotlin/at/rueckgr/spotlight/service/ImageDownloaderService.kt
+++ src/main/kotlin/at/rueckgr/spotlight/service/ImageDownloaderService.kt
@@ -3,15 +3,15 @@ package at.rueckgr.spotlight.service
 import at.rueckgr.spotlight.model.DownloadableImage
 import at.rueckgr.spotlight.util.SpotlightException
 import org.apache.commons.io.IOUtils
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
+import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.impl.classic.HttpClients
 
 class ImageDownloaderService {
     fun fetchImage(downloadableImage: DownloadableImage): ByteArray {
         val httpClient = HttpClients.custom().disableDefaultUserAgent().build()
 
         httpClient.execute(HttpGet(downloadableImage.url)).use {
-            val statusCode = it.statusLine.statusCode
+            val statusCode = it.code
             if (statusCode != 200) {
                 throw SpotlightException(String.format("Fail to retrieve image content: %s", statusCode))
             }
